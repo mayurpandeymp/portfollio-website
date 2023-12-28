ae3db90f4ee5615894bb6929ce6791717b0db598
Mayur Pandey <152061192+mayurpandeymp@users.noreply.github.com>
2023-12-28 21:37:30 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+    <!--Setup and start animation! -- >
+    <script>
+        var typed = new Typed('#element', {
+            strings: ['Web Developer'],
+        typeSpeed: 50,
+        });
+    </script>
